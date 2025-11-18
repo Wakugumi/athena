31b59433fc533c51f50248bb5aae5ae478c5eeb3
Ananda Risyad <anandarisyad@proton.me>
2025-11-19 05:34:32 +0700

    feature: deployment website
    Dockerfile.
    fix: Next config and package sync.
    GitHub Action.

--- apps/athena-website/next.config.ts
+++ apps/athena-website/next.config.ts
@@ -2,10 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-<<<<<<< HEAD
   distDir: 'dist'
-=======
->>>>>>> fc6180d68ed08d0397fb333b5c79812994939205
 };
 
 export default nextConfig;
